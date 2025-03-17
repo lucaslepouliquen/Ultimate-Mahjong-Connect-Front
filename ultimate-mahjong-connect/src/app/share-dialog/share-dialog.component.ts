95d6deada9fbe43c67225badf269d92d5e3931e8
Lucas Le Pouliquen <lucaslepouliquen@outlook.fr>
2025-03-17 22:16:14 +0100

    Add share dialog logic

--- ultimate-mahjong-connect/src/app/share-dialog/share-dialog.component.ts
+++ ultimate-mahjong-connect/src/app/share-dialog/share-dialog.component.ts
@@ -3,15 +3,55 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-share-dialog',
-  imports: [MatDialogModule, MatInputModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatDialogModule, MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './share-dialog.component.html',
   styleUrl: './share-dialog.component.css'
 })
 export class ShareDialogComponent {
+  isCopied: boolean = false;
+
   share(method:string) {
-    console.log('Shared!');
+    var url = window.location.href;
+
+    switch(method) {
+      case 'facebook':
+        window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`, '_blank');
+        break;
+      case 'messenger':
+        window.open(`https://www.facebook.com/dialog/send?link=${encodeURIComponent(url)}&app_id=YOUR_APP_ID`, '_blank');
+        break;
+      case 'whatsapp':
+        window.open(`https://api.whatsapp.com/send?text=${encodeURIComponent(url)}`, '_blank');
+        break;
+      case 'threads':
+        // Implements Threads sharing logic
+        break;
+      case 'twitter':
+        window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}`, '_blank');
+        break;
+      case 'copy':
+        this.copyToClipboard();
+        break;
+      default:
+        console.error('Invalid sharing method');
+    }
+  }
+
+  copyToClipboard(): boolean{
+    navigator.clipboard.writeText(window.location.origin + window.location.pathname)
+      .then(() => {
+        console.log('Texte copié!');
+        this.isCopied = true;
+        setTimeout(() => this.isCopied = false, 2000);
+      })
+      .catch(err => {
+        console.error('Erreur lors de la copie: ', err);
+      });
+    this.isCopied = true
+    return true
   }
 }
